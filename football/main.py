ed06d136fba405f0e60fa0118f6970e1ef1ef8d3
Dante Beltran Nanziot <dantebelnan@gmail.com>
2024-04-05 13:15:35 -0300

    cargada una obtención de datos

--- football/main.py
+++ football/main.py
@@ -5,14 +5,33 @@ url = "https://api.football-data.org/v4/"
 
 response = request.get(url+"areas")
 
-data = response.text
+data = json.loads(response.text)
 
-data = json.loads(data)
 
-print (data["count"])
-
+argId = 0
 for area in data["areas"]:
     if area["name"] == "Argentina":
-        print(area)
-    
+        #print(area)
+        argId = area["id"]
+
+response = request.get(url+"competitions/?areas="+str(argId))
+
+data = json.loads(response.text)
+
+
+compId = 0
+for comp in data["competitions"]:
+    #print(comp)
+    if comp["name"] == "Liga Profesional":
+        compId = comp["id"]
+#print(compId)
+
+response = request.get(url+"competitions/"+str(compId)+"/teams/?season=2023")
+data = json.loads(response.text)
+
+
+print(data["message"])
+for info in data:
+    print(info)
+
     
